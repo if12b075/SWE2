2a7a4182d0f663f4e41d255c0737ecd865246e6c
if12b013 <if12b013@technikum-wien.at>
2014-05-06 21:44:05 +0200

    neue Schnittstellen hinzugefügt (UpdatePerson, UpdateRechnung)

--- SWE_Schnittstelle/src/commons/CommandRequestTitel.java
+++ SWE_Schnittstelle/src/commons/CommandRequestTitel.java
@@ -4,7 +4,11 @@ public class CommandRequestTitel {
 	public static final String GET_ALLE_KONTAKTE = "GetAlleKontakte";
 	public static final String GET_ALLE_RECHNUNGEN = "GetAlleRechnungen";
 	public static final String GET_FILTER_KONTAKTE = "GetKontaktFilter";
-	public static final String GET_FILTER_RECHNUNG = "GetRechnungFilter";
+	public static final String GET_FILTER_RECHNUNG = "GetRechnungFilter"; // Jasmin: Implementierung fehlt noch
 	public static final String GET_PERSON_BY_ID = "GetPersonById";
 	public static final String GET_RECHNUNG_BY_ID = "GetRechnungById";
+	
+	// Jasmin - Neu:
+	public static final String UPDATE_RECHNUNG = "UpdateRechnung";
+	public static final String UPDATE_PERSON = "UpdatePerson";
 }
