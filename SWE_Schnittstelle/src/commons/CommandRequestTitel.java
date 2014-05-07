252da0894566648498166549d11f6cdbd9470c67
if12b013 <if12b013@technikum-wien.at>
2014-05-07 17:59:38 +0200

    neue Commands hinzugefügt

--- SWE_Schnittstelle/src/commons/CommandRequestTitel.java
+++ SWE_Schnittstelle/src/commons/CommandRequestTitel.java
@@ -6,9 +6,9 @@ public class CommandRequestTitel {
 	public static final String GET_FILTER_KONTAKTE = "GetKontaktFilter";
 	public static final String GET_FILTER_RECHNUNG = "GetRechnungFilter"; // Jasmin: Implementierung fehlt noch
 	public static final String GET_PERSON_BY_ID = "GetPersonById";
+	public static final String GET_FIRMA_BY_ID = "GetFirmaById";
 	public static final String GET_RECHNUNG_BY_ID = "GetRechnungById";
-	
-	// Jasmin - Neu:
 	public static final String UPDATE_RECHNUNG = "UpdateRechnung";
 	public static final String UPDATE_PERSON = "UpdatePerson";
+	public static final String UPDATE_FIRMA = "UpdateFirma";
 }
